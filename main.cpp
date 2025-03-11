d8244048d00bb82e83633da46284dc4939e1eb6d
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-03-11 23:02:39 +0100

    feat: add hitSphere()

--- main.cpp
+++ main.cpp
@@ -8,7 +8,23 @@ import ray;
 
 using namespace std;
 
+bool hitSphere(const Vec3& center, float radius, const Ray& r) {
+
+    Vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0f * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+
+}
+
 Vec3 color(const Ray &r) {
+    
+    if (hitSphere(Vec3(0, 0,-1), 0.5, r)) {
+        return Vec3(1, 0, 0);
+    }
+
     Vec3 unitDirection = Vec3::unitVector(r.direction());
     float t = 0.5 * (unitDirection.y() + 1.0f);
     return (1.0 - t) * Vec3(1.0f, 1.0f, 1.0f) + t * Vec3(0.5f, 0.7f, 1.0f);
@@ -24,7 +40,7 @@ int main() {
     buffer << "P3" << endl << nx << " " << ny << endl << 255 << endl;
 
     Vec3 lowerLeftCorner(-2.0f, -1.0f, -1.0f);
-    Vec3 horizontal(8.0f, 0.0f, 0.0f);
+    Vec3 horizontal(4.0f, 0.0f, 0.0f);
     Vec3 vertical(0.0f, 2.0f, 0.0f);
     Vec3 origin(0.0f, 0.0f, 0.0f);
 

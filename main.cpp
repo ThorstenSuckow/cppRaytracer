c7cc6b5bc2021360865d1769914805a24ff81bd4
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-03-16 21:45:33 +0100

    feat: update main.cpp with two hitables

--- main.cpp
+++ main.cpp
@@ -4,38 +4,27 @@
 import IOUtil;
 import vecmath;
 import ray;
-
+import sphere;
+import hitable;
 
 using namespace std;
 
-float hitSphere(const Vec3& center, float radius, const Ray& r) {
 
-    Vec3 oc = r.origin() - center;
-    float a = dot(r.direction(), r.direction());
-    float b = 2.0f * dot(oc, r.direction());
-    float c = dot(oc, oc) - radius * radius;
-    float discriminant = b * b - 4 * a * c;
-
-    if (discriminant < 0) {
-        return -1.0f;
-    }
-
-    return ( -b - sqrt(discriminant) ) / (2.0f * a);
-}
-
-Vec3 color(const Ray &r) {
+Vec3 color(const Ray &r, Hitable *world) {
     
-    float t = hitSphere(Vec3(0, 0, -1), 0.5, r);
+    hitRecord rec;
+    const float maxFloat = numeric_limits<float>::max();
 
-    if (t > 0.0f) {
-        Vec3 N = Vec3::unitVector(
-            r.pointAtParameter(t) - Vec3(0.0f, 0.0f, -1.0f)
+    if (world->hit(r, 0.0f, maxFloat, rec)) {
+        return 0.5f * Vec3(
+            rec.normal.x() + 1.0f, 
+            rec.normal.y() + 1.0f, 
+            rec.normal.z() + 1.0f
         );
-        return 0.5f * Vec3(N.x() + 1.0f, N.y() + 1.0f, N.z() + 1.0f);
     }
 
     Vec3 unitDirection = Vec3::unitVector(r.direction());
-    t = 0.5f * (unitDirection.y() + 1.0f);
+    float t = 0.5f * (unitDirection.y() + 1.0f);
     return (1.0f - t) * Vec3(1.0f, 1.0f, 1.0f) + t * Vec3(0.5f, 0.7f, 1.0f);
 }
 
@@ -53,29 +42,30 @@ int main() {
     Vec3 vertical(0.0f, 2.0f, 0.0f);
     Vec3 origin(0.0f, 0.0f, 0.0f);
 
+    Hitable* list[2];
+    list[0] = new Sphere(Vec3(0.0f, 0.0f, -1.0f), 0.5f);
+    list[1] = new Sphere(Vec3(0.0f, -100.5f, -1.0f), 100.0f);
+
+    Hitable* world = new HitableList(list, 2);
+
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
 
             float u = float(i) / float(nx);
             float v = float(j) / float(ny);
-            
 
             Ray r(origin, lowerLeftCorner + u * horizontal + v * vertical);
 
-            Vec3 col = color(r);
-
+            Vec3 col = color(r, world);
 
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
             int ib = int(255.99 * col[2]);
 
             buffer << ir << " " << ig << " " << ib << endl;
-
         }
     }
 
-    
     //cout << buffer.str();
     IOUtil::putContents("demo.ppm", buffer.str());
-
 }
